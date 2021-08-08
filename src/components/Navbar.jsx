8bf8eef8c84a0a16199c7364386c0ef6fe3f3fe6
jasonrobwebster <jasonrobwebster@gmail.com>
2021-08-08 23:38:29 +0200

    messing with font sizes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Nav = styled.nav`
   align-items: center;
 
   width: 100%;
-  height: 4rem;
+  height: 3rem;
 
   background-color: ${(props) => props.theme.bg};
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.15);
@@ -35,13 +35,13 @@ Nav.Menu = styled.div`
   }
 
   & > *:not(:last-child) {
-    margin-right: 2.5rem;
+    margin-right: 2.2rem;
   }
 `
 
 Nav.Link = styled(NavLink)`
   color: ${(props) => props.theme.text.color};
-  font-size: 1.2rem;
+  font-size: 1.1rem;
   font-weight: bold;
   text-decoration: none;
 `
