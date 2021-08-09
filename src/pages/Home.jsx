b54322dc7f53bfb1f30673d286e2db6107da15a0
jasonrobwebster <jasonrobwebster@gmail.com>
2021-08-09 20:45:25 +0200

    added initial home page content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,49 @@
 import React from "react"
 
+import PropTypes from "prop-types"
+
 import { HyphenatedText } from "../components/text"
+
+const A = ({ children, ...props }) => {
+  return (
+    <a {...props} target="_blank">
+      {children}
+    </a>
+  )
+}
+A.propTypes = { children: PropTypes.string.isRequired }
 
 const Home = () => {
   return (
     <HyphenatedText className="Home__Content">
       <h1>About me</h1>
-      <p>Hi, I&apos;m Jason.</p>
+      <p>
+        Hi, I&apos;m Jason. Personally, I&apos;m a software and science loving
+        gamer geek. I&apos;m into maths, physics, game design, tech, and am
+        currently playing an unhealthy amount of &ldquo;Hollow Knight&rdquo; and
+        &ldquo;Legends of Runeterra&rdquo;. I obtained an MSc in Physics with
+        the goal of becoming an academic before making the decision to switch to
+        industry. Professionally, I&apos;m a full stack engineer and data
+        scientist at <A href="https://kandua.com">Kandua</A>. I am also learning
+        the ropes in product management. In my day to day I use tools like
+        React, Express, GCP, PostgreSQL, and Python to build{" "}
+        <A href="https://kandua.com">Kandua</A> and gain insights into what its
+        customer base is doing on the platform. I have previously been a data
+        science tutor at <A href="https://explore-datascience.net/">Explore</A>{" "}
+        and a consultant at <A href="https://percept.co.za">Percept</A>.
+      </p>
+      <h1>This blog</h1>
+      <p>
+        I built this blog as a means of sharing what I have learnt about data
+        science, tech, stats, and anything else I enjoy really. I built and
+        designed it from scratch using React, styled-components, Webpack, and
+        other tools (largely to teach myself how to build a blog from scratch).
+        It is hosted on github using github pages at{" "}
+        <A href="https://github.com/jasonrobwebster/jasonrobwebster.github.io">
+          this repo
+        </A>
+        .
+      </p>
     </HyphenatedText>
   )
 }
