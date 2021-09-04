9103a7481c9d60da377220be00c85916dc0bfaf9
jasonrobwebster <jasonrobwebster@gmail.com>
2021-09-04 18:02:55 +0200

    change page width

--- src/shared/styles/PageContent.js
+++ src/shared/styles/PageContent.js
@@ -2,8 +2,8 @@ import styled from "styled-components"
 
 const PageContent = styled.div`
   width: 90%;
-  max-width: 800px;
-  min-height: calc(100vh - 3rem); // vh - navbarHeight - footerHeight
+  max-width: 630px;
+  min-height: calc(100vh - 3rem); // view height - navbarHeight
   margin-left: auto;
   margin-right: auto;
   padding-top: 2.5rem;
