da868edec9cbef165282345874e407950ffb3c52
jasonrobwebster <jasonrobwebster@gmail.com>
2021-09-05 14:50:49 +0200

    linted file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,39 @@
 module.exports = {
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ],
-  "plugins": [
-    "react",
-  ],
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
-    }
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  plugins: ["react"],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-  "env": {
-    "browser": true,
-    "node": true,
+  env: {
+    browser: true,
+    node: true,
   },
-  "rules": {
-    "sort-imports": ["error", {
-      "ignoreCase": true,
-      "ignoreDeclarationSort": false,
-      "ignoreMemberSort": false,
-      "memberSyntaxSortOrder": ["none", "all", "single", "multiple"],
-      "allowSeparatedGroups": true
-    }],
-    "indent": ["error", 2, {
-      "flatTernaryExpressions": true,
-      "offsetTernaryExpressions": false
-    }],
+  rules: {
+    "sort-imports": [
+      "error",
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ["none", "all", "single", "multiple"],
+        allowSeparatedGroups: true,
+      },
+    ],
+    indent: [
+      "error",
+      2,
+      {
+        flatTernaryExpressions: true,
+        offsetTernaryExpressions: false,
+      },
+    ],
     "comma-dangle": ["error", "only-multiline"],
-    "quotes": ["error", "double"],
-    "semi": ["error", "never"],
-    "object-curly-spacing": ["error", "always"]
-  }
+    quotes: ["error", "double"],
+    semi: ["error", "never"],
+    "object-curly-spacing": ["error", "always"],
+    "react/no-unescaped-entities": "off",
+  },
 }
